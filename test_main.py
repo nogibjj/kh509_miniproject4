a26b3eebebe6db955ff8ac1a2cb877f21c63d41e
katelyn-hucker <143521756+katelyn-hucker@users.noreply.github.com>
2023-09-06 20:01:09 +0000

    try3

---
+++ test_main.py
@@ -0,0 +1,10 @@
+from stat import calculate_mean
+import pandas as pd
+
+data = {'Height': [5.1, 6.2, 5.1, 5.2]}
+
+testing_df = pd.DataFrame(data)
+
+
+def test_mean():
+    assert calculate_mean(testing_df['Height']) == 5.4
