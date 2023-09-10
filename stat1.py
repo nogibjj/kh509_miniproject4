1e3eb394e69f908d32aba019d6eeb59b25eda2b7
katelyn-hucker <143521756+katelyn-hucker@users.noreply.github.com>
2023-09-10 10:46:25 -0400

    Added data visualization

--- stat1.py
+++ stat1.py
@@ -1,9 +1,14 @@
 import pandas as pd 
+import matplotlib.pyplot as plt
 
 df1 = pd.read_csv('iris.csv')
 
-def calculate_mean(dataset_col):
-    out=dataset_col.mean()
+def calc_desc_stat(dataset_col):
+    out=dataset_col.describe()
     return out
 
-print(calculate_mean(df1['petal.length']))
+print(calc_desc_stat(df1['petal.length']))
+
+def boxplot_of_col(dataset_col):
+    boxplot = dataset_col.boxplot()
+    return boxplot
